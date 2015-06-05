1197542dff422d8bea97562e6d4dd63ef25ee10d
JellyChoi <chm073@gmail.com>
2015-06-05 13:55:32 +0900

    jellyblog model 생성 (Document, Category)

--- jellyblog/models.py
+++ jellyblog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Document(models.Model):
+	document_id = models.AutoField()
+	category_id = models.ForeignKey(Category)
+	document_title = models.CharField(max_length=100)
+	document_content = models.TextField()
+	document_time = models.DateTimeField()
+
+
+class Category(models.Model):
+	category_id = models.AutoField()
+	category_parent_id = models.IntegerField()
+	category_name = models.CharField(max_length=20)
