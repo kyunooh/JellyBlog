9477c476615a69c59e4d58275c1b1a2717ce258c
JellyChoi <chm073@gmail.com>
2015-06-05 14:12:43 +0900

    model django 형식에 맞게 수정

--- jellyblog/models.py
+++ jellyblog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	category_id = models.AutoField(primary_key=True)
+	category_parent_id = models.IntegerField(null=True)
+	category_name = models.CharField(max_length=20)
+
+
+
 class Document(models.Model):
-	document_id = models.AutoField()
+	document_id = models.AutoField(primary_key=True)
 	category_id = models.ForeignKey(Category)
 	document_title = models.CharField(max_length=100)
 	document_content = models.TextField()
 	document_time = models.DateTimeField()
-
-
-class Category(models.Model):
-	category_id = models.AutoField()
-	category_parent_id = models.IntegerField()
-	category_name = models.CharField(max_length=20)
