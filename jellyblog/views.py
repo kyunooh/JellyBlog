0ae16f61fff613569c564e43b202631fdcb7a014
JellyChoi <chm073@gmail.com>
2015-06-06 17:22:02 +0900

    views.py 렌더링 부분 수정

--- jellyblog/views.py
+++ jellyblog/views.py
@@ -1,21 +1,23 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 from .models import Category, Document
 
+
 def index(request):
-	latest_document_list = Document.objects.order_by('-document_id')[:5]
-	template = loader.get_template('jellyblog/index.html')
-	context = RequestContext(request, {
-		'latest_document_list' : latest_document_list,
-	})
-	return HttpResponse(template.render(context))
+    latest_document_list = Document.objects.order_by('-document_id')[:5]
+    context = {'latest_document_list': latest_document_list}
+    return render(request, 'jellyblog/index.html', context)
 
 
 def detail(request, document_id):
-	return HttpResponse("Detail page %s." % document_id)
+    try:
+        document = Document.objects.get(pk=document_id)
+    except Document.DoesNotExist:
+        raise Http404("Document isn't exist")
+    return render(request, 'jellyblog/detail.html', {'document': document})
 
 
 def category(request, category_id):
-	return HttpResponse("Categoyr page %s." % category_id)
+    return HttpResponse("Categoyr page %s." % category_id)
 
