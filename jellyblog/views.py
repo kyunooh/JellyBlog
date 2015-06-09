5c8267a22ac67baab2a0510e0b760e7261626f0f
JellyChoi <chm073@gmail.com>
2015-06-10 06:47:14 +0900

    카테고리 부분 소스 적용(추후 상위,하위 항목 분리 필요)

--- jellyblog/views.py
+++ jellyblog/views.py
@@ -1,20 +1,22 @@
-from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-
 from .models import Category, Document
 
 
+category_list = Category.objects.all()
+
 def index(request):
     latest_document_list = Document.objects.order_by('-document_id')[:5]
-    context = {'latest_document_list': latest_document_list}
+    context = {'latest_document_list': latest_document_list,'category_list' : category_list }
     return render(request, 'jellyblog/index.html', context)
 
 
 def detail(request, document_id):
     document = get_object_or_404(Document, pk=document_id)
-    return render(request, 'jellyblog/detail.html', {'document': document})
+    return render(request, 'jellyblog/detail.html', {'document': document, 'category_list': category_list})
 
 
 def category(request, category_id):
-    return HttpResponse("Categoyr page %s." % category_id)
+    document = get_object_or_404(Category, pk=category_id)
+    return render(request, 'jellyblog/category.html',{'category_list':category_list})
+
 
