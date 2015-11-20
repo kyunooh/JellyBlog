db3df17610ee149fa9e14588a7545c493026a219
ChoiHyeonMook <JellyChoi@ChoiHyeonMooks-MacBook-Air.local>
2015-11-20 23:37:57 +0900

    add NoteViewTest

--- jellyblog/tests.py
+++ jellyblog/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Note
 
 # Create your tests here.
+class NoteViewTest(TestCase):
+
+    def get_note(self):
+        Note.create(content='note 1')
+        
