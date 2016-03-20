c0c618a06fcf0f58683b4796de115ad612c5530f
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-03-20 23:07:31 +0900

    add Message feature

--- about_me/models.py
+++ about_me/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+
+class Message(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.CharField(max_length=150)
+    content = models.TextField(null=False)
+
+    def __str__(self):
+        return self.name
--- about_me/views.py
+++ about_me/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Message
 
 def index(request):
+    if request.method == "POST":
+        message = Message()
+        message.name = request.POST['name']
+        message.email = request.POST['email']
+        message.content = request.POST['content']
+        message.save()
     return render(request, 'about_me/index.html')
