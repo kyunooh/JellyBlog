b808394cddd51ad7d7f9fd3bdb44ed9deb7a16c4
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-03-22 22:59:24 +0900

    remove not useful comment

--- jellyblog/models.py
+++ jellyblog/models.py
@@ -6,22 +6,11 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Category(models.Model):
-    # 카테고리 Model
-    # 모든 카테고리는 상위 카테고리(parent)를 가지고 있다. (자기 참조)
-    # parent의 id가 1일 경우 상위 카테고리,
-    # 1이 아닌 경우 하위 카테고리로 분류한다.
-    # (참고로 1은 사이트 최초 접속시 생성되며 Home 값을 가진다.
-    # 상세내용은 views.py의 상단부분과 sorted_category()함수 참고)
     id = models.AutoField(primary_key=True)
     parent = models.ForeignKey('self')
     name = models.CharField(max_length=20)
 
     def __init__(self, *args, **kwargs):
-        # 상위 카테고리에서 하위 카테고리 리스트를 가지기 위해
-        # 초기화시 children 리스트를 추가, (상세내용은 views.py의 sorted_category()함수 참고
-        #
-        # 생성자에 list를 선언하지않고 Category에 바로 선언할 경우
-        # 모든 카테고리가 list를 공유하는 이슈(자바상의 static 변수와 같이 동작한다)가 발생하기 때문에 생성자에 선언
         super(Category, self).__init__(*args, **kwargs)
         self.children = []
 
@@ -30,25 +19,16 @@ class Category(models.Model):
 
     @classmethod
     def sorted_category(cls):
-        # 카테고리를 정렬하기 위한 함수,
-        # 상위 카테고리는 남겨두고, 하위 카테고리의 경우 상위 카테고리(parent)의 자식(children)으로 집어넣는다.
-        # 추후 리팩토링 예정
-        # 전체 카테고리를 불러온뒤 리스트로 변환한다.
         return_category = list(cls.objects.all())
-        childList = []  # 하위 카테고리를 담을 리스트 생성
+        childList = []
         for category in return_category:
-            # 전체 카테고리 리스트를 반복 하면서 상위 카테고리의 경우(parent == 1) 아무처리도 하지 않는다.
             if (category.parent.id == 1):
                 continue
             else:
-                # 상위 카테고리 위치를 반환한다.
                 parent_index = return_category.index(category.parent)
-                # 상위 카테고리의 children 리스트에 요소추가
                 return_category[parent_index].children.append(category)
-                # 하위 카테고리 리스트에 담음
                 childList.append(category)
 
-        # 카테고리 리스트에서 하위 카테고리의 값들은 삭제
         for child in childList:
             return_category.remove(child)
 
--- jellyblog/views.py
+++ jellyblog/views.py
@@ -20,10 +20,6 @@ def index(request):
 
 @minified_response
 def index_with_page(request, page):
-    """
-    모든 문서를 가져와 리스트형태로 바꾼뒤 페이지네이션하여
-    해당 페이지의 문서 리스트를 반환한다.
-    """
     document_list = Document.objects.all().filter(public_doc=True).order_by('-id')
     paginator = Paginator(document_list, 4)
     documents = get_documents(paginator, page)
@@ -43,15 +39,11 @@ def category_detail(request, category_id):
 
 @minified_response
 def category_with_page(request, category_id, page):
-    """
-    선택된 카테고리에 대한 문서를 가져온뒤 페이지네이하여
-    해당 페이지의 문서리스트를 반환한다.
-    """
-    selectedCategory = Category.objects.get(id=category_id)
+    selected_category = Category.objects.get(id=category_id)
     document_list = []
-    if selectedCategory.parent.id == 1:
+    if selected_category.parent.id == 1:
         # 카테고리가 상위 카테고리인지 아닌지를 판별 후, 상위 카테고리일 경우엔 하위 카테고리의 문서 리스트를 추가함
-        children = Category.objects.all().filter(parent=selectedCategory.id)
+        children = Category.objects.all().filter(parent=selected_category.id)
         for child in children:
             document_list += Document.objects.all() \
                 .filter(category_id=child.id, public_doc=True)
@@ -66,17 +58,13 @@ def category_with_page(request, category_id, page):
         'category_id': category_id,
         'page_range': get_page_number_range(
             paginator, documents),
-        'category_name': selectedCategory.name,
+        'category_name': selected_category.name,
     }
     return render(request, 'jellyblog/category.html', context)
 
 
 @minified_response
 def detail(request, document_id):
-    """
-    document_id에 해당하는 문서를 가져오고, 해당 문서가 존재하지 않을경우 404페이지를 반환한다.
-    함수를 호출할때 마다 해당 문서의 조회수를 1올려주고 반환한다.
-    """
     document = get_object_or_404(Document, pk=document_id)
     document.read()
     return render(request, 'jellyblog/detail.html',
