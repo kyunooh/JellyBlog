bdf90935ca87ebac69221e0b1a3f666228d56503
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-03-30 15:38:41 +0900

    add note and update template

--- jellyblog/static/jellyblog/js/angular/jellyblog-app.js
+++ jellyblog/static/jellyblog/js/angular/jellyblog-app.js
@@ -1,11 +1,32 @@
 /**
  * 추후 기능 추가를 위해 생성해 둠
  */
-var app = angular.module("JellyBlog",[]);
+var app = angular.module("jellyBlog",['slickCarousel']);
 app.controller("MainCtrl", [
-    $scope,
-    $http,
+    '$scope',
+    '$http',
     function($scope, $http) {
+        $scope.notes = [];
 
+        $scope.slickConfig = {
+            enabled: true,
+            autoplay: false,
+            draggable: false,
+            method: {},
+            event: {
+                beforeChange: function (event, slick, currentSlide, nextSlide) {
+                },
+                afterChange: function (event, slick, currentSlide, nextSlide) {
+                }
+            }
+        };
+
+        $scope.noteInit = function () {
+            $http
+                .get("/api/notes/")
+                .then(function successCallback(response) {
+                    $scope.notes = response.data;
+                });
+        };
     }
 ]);
