aec44c59b1c4711e1a0d08bb8bb3b85505d672a6
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-06 05:14:12 +0900

    remove about compressor and ckeditor

--- blogadmin/settings.py
+++ blogadmin/settings.py
@@ -42,9 +42,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'ckeditor',
-    'ckeditor.fields',
-    'compressor',
     'favicon',
 
     'jellyblog',
