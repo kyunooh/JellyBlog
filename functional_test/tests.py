2468ee878b3c8f3a55a4ee5e550e600afe1bfae6
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-08 21:53:30 +0900

    complete Message test code

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,8 +1,14 @@
 import unittest
 from selenium import webdriver
+from about_me.models import Message
 
 
 class MessageTest(unittest.TestCase):
+    def setUp(self):
+        self.test_name = 'functional test name'
+        self.test_email = 'functional@test.email'
+        self.test_content = 'functional test message content'
+
     def test_leave_message(self):
         # 유저는 메인 화면으로 들어와서
         self.browser = webdriver.Firefox()
@@ -14,8 +20,19 @@ class MessageTest(unittest.TestCase):
         about_me_button.click()
         self.assertIn('젤리의 망상 - About me', self.browser.title)
         # Message Form을 확인 후 메시지를 남긴다.
+        name = self.browser.find_element_by_name('name')
+        name.send_keys(self.test_name)
 
+        email = self.browser.find_element_by_name('email')
+        email.send_keys(self.test_email)
+
+        content = self.browser.find_element_by_name('content')
+        content.send_keys(self.test_content)
+
+        self.browser.find_element_by_tag_name('form').submit()
         # Message가 서버에 저장된다.
+        # 다시 about me page로 돌아온다.
+        self.assertIn('젤리의 망상 - About me', self.browser.title)
 
         # 메시지를 남기고 브라우저 종료
         self.browser.quit()
