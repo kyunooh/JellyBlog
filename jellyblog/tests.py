2b7adfcd2c31cfea5ac686ed9f57d94388ed1be1
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-09 18:40:23 +0900

    add jellyblog note view test

--- jellyblog/tests.py
+++ jellyblog/tests.py
@@ -1,9 +1,20 @@
+from django.http import HttpRequest
 from django.test import TestCase
+
+from jellyblog.views import index_with_page
 from .models import Note
 
-# Create your tests here.
-class NoteViewTest(TestCase):
 
-    def get_note(self):
-        Note.create(content='note 1')
-        
+class NoteViewTest(TestCase):
+    def setUp(self):
+        self.note_content1 = "note test 111"
+        self.note_content2 = "note test 222"
+        Note.objects.create(content=self.note_content1)
+        Note.objects.create(content=self.note_content2)
+
+    def test_get_notes(self):
+        request = HttpRequest()
+        response = index_with_page(request)
+
+        self.assertIn(self.note_content1, response.content.decode())
+        self.assertIn(self.note_content2, response.content.decode())
