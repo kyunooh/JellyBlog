d1a078a1f6bddb89a75ec1b9d53f693ba2be0e45
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-10 20:12:23 +0900

    add private doc test

--- jellyblog/tests.py
+++ jellyblog/tests.py
@@ -21,15 +21,27 @@ class NoteViewTest(TestCase):
 
 
 class DocumentViewTest(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        super(DocumentViewTest, cls).setUpClass()
         Category.init_category()
+
+    def setUp(self):
         category = Category.objects.get(pk=1)
         self.test_doc1 = Document.objects.create(
             category=category,
-            title="Document Test",
-            content="Content content content ",
+            title="Document Test1",
+            content="Content content content",
             meta_tag="metametametametametameta",
             public_doc=True
+        )
+
+        self.test_doc2 = Document.objects.create(
+            category=category,
+            title="Document Test2",
+            content="Content2 content2 content2",
+            meta_tag="metametametametametameta",
+            public_doc=False
         )
 
     def test_get_document(self):
@@ -37,5 +49,12 @@ class DocumentViewTest(TestCase):
         response = index(request)
 
         self.assertIn(self.test_doc1.title, response.content.decode())
+        self.assertIn(self.test_doc1.content, response.content.decode())
+
+    def test_about_private_doc(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertNotIn(self.test_doc2.title, response.content.decode())
+        self.assertNotIn(self.test_doc2.content, response.content.decode())
 
 
