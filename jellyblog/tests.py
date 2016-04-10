de8f85f147309e9e9a895412b0b3a8b1bcdfac7b
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-10 20:01:04 +0900

    add document view test

--- jellyblog/tests.py
+++ jellyblog/tests.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 
 from jellyblog.views import index
-from .models import Note
+from .models import Category, Note, Document
 
 
 class NoteViewTest(TestCase):
@@ -18,3 +18,24 @@ class NoteViewTest(TestCase):
 
         self.assertIn(self.note_content1, response.content.decode())
         self.assertIn(self.note_content2, response.content.decode())
+
+
+class DocumentViewTest(TestCase):
+    def setUp(self):
+        Category.init_category()
+        category = Category.objects.get(pk=1)
+        self.test_doc1 = Document.objects.create(
+            category=category,
+            title="Document Test",
+            content="Content content content ",
+            meta_tag="metametametametametameta",
+            public_doc=True
+        )
+
+    def test_get_document(self):
+        request = HttpRequest()
+        response = index(request)
+
+        self.assertIn(self.test_doc1.title, response.content.decode())
+
+
