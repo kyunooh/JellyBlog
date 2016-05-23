4515c8fa003f9acef48a659a348ad30098ef3a50
ChoiHyeonMook <chm073@gmail.com>
2016-05-23 22:34:53 +0900

    add test code(temp)

--- lifeblog/tests.py
+++ lifeblog/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from .models import Document
 
-# Create your tests here.
+
+class DocumentTest(LiveServerTestCase):
+    def setUp(self):
+        test_public_doc = Document.objects.create(
+            title="DocumentTest Title",
+            content="DocumentTest Content content",
+            meta_tag="metametametametametametametameta",
+            public_doc=True)
+        
+        test_private_doc = Document.objects.create(
+            title="DocumentTest private",
+            content="DocumentTest Content content",
+            meta_tag="metametametametametametametameta",
+            public_doc=False)
+
+    def test_public_doc(self):
+        pass
